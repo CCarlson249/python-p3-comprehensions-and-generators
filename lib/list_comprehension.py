e5b8120505cd5841f5296722af115309a9748f79
Clayton Carlson <claytonrc97@gmail.com>
2023-03-15 22:52:31 -0700

    lab complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    return [x for x in num_list if x % 2 == 0]
     pass
 
-def make_exclamation(sentence_list):
+def make_exclamation(sentences):
+    return [sentence.strip() + "!" for sentence in sentences]
+
     pass
